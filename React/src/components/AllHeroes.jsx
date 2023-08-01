46ac5b34cb0c437f47b97dcfa1cfb2e82ec13f92
AndySmithQA <111853853+AndySmithQA@users.noreply.github.com>
2023-08-01 12:31:08 +0100

    Add files via upload

---
+++ React/src/components/AllHeroes.jsx
@@ -0,0 +1,46 @@
+import Row from 'react-bootstrap/Row' 
+import React, { useEffect, useState } from 'react'
+import HeroDetail from './HeroDetail'
+import { getAllHeroes } from '../services/hero-service';
+
+const AllHeroes = () => {
+
+    const [total, setTotal] = useState(0);
+
+    const totalCountHandler = (name) => {
+        console.log(name)
+        setTotal(total+1)
+    }
+
+    const [heroes, setHeroes] = useState([]);
+
+    useEffect(()=>{
+      getHeroesFromAPI();
+    }, []);
+
+    const getHeroesFromAPI = () =>{}
+
+    getAllHeroes()
+      .then(res => {
+        setHeroes(res.data);
+      })
+      .catch(err => {
+        setHeroes([]);
+        console.log(err)
+      })
+
+  return (
+    <>
+    <h1>Total Count is: {total}</h1>
+    <Row>
+    {heroes.map(hero=>(
+      <div className='container' key={hero.id.toString()}>
+        <HeroDetail info={hero} parentCount={totalCountHandler} />
+      </div>
+    ))}
+ </Row>
+ </>
+  )
+}
+
+export default AllHeroes
